4c63dc7a6b6bb1d339aef820504405510bd8d549
Tomáš Šromovský <tomas.sromovsky@posam.sk>
2024-09-25 16:23:36 +0200

    native-federation projects update

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {loadRemoteModule} from '@angular-architects/native-federation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'users',
+    loadComponent: () =>
+      loadRemoteModule('users', './Component').then((m) => m.AppComponent),
+  }
+];
